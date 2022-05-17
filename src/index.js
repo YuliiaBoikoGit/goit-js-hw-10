39a7909ba5ee24c1f59385e1761ee487d9366c7d
Yuliia Boiko <littlehappinies@gmail.com>
2022-05-17 12:11:44 +0300

    Update fetch function

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ function onSearch(event) {
 
     if (countryName === '') {
         resetCountryList();
+        return;
     };
 
     fetchCountries(countryName)
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -2,7 +2,13 @@ const BASE_URL = 'https://restcountries.com/v2';
 const searchParams = 'fields=name,capital,population,flags,languages'
 
 function fetchCountries(name) {
-    return fetch(`${BASE_URL}/name/${name}?${searchParams}`).then(response => response.json());
+    return fetch(`${BASE_URL}/name/${name}?${searchParams}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+            return response.json();
+        });
 };
 
 export { fetchCountries };
